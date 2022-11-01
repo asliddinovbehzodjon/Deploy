b677ab017ca99bb1ffd458b09520e5a4501e152e
Behzod <behzodtuit@gmail.com>
2022-11-01 22:14:34 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
